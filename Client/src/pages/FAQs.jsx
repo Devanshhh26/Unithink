f593fea14ef32c91b875a7d8e8de67fbd496106f
Devansh Mehta <devanshmehta26@gmail.com>
2024-02-28 23:47:36 +0530

    Added FAQs section

--- Client/src/pages/FAQs.jsx
+++ Client/src/pages/FAQs.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import { Accordion } from 'flowbite-react';
 
 function FAQs() {
   return (
-    <div>FAQs</div>
+    <div className='pt-24'>
+    <h1 className='mx-auto text-3xl font-bold  text-center p-5'>Frequently Asked Questions</h1>
+      <Accordion collapseAll className='mr-5 ml-5'>
+      <Accordion.Panel>
+        <Accordion.Title>What is Unithink?</Accordion.Title>
+        <Accordion.Content>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque amet sint id dolore harum nihil aliquid non totam eaque sequi. Delectus illum cumque a earum suscipit ex autem, accusamus maxime.
+          </p>
+        </Accordion.Content>
+      </Accordion.Panel>
+      <Accordion.Panel>
+        <Accordion.Title>Is there a Figma file available?</Accordion.Title>
+        <Accordion.Content>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+            Flowbite is first conceptualized and designed using the Figma software so everything you see in the library
+            has a design equivalent in our Figma file.
+          </p>
+        </Accordion.Content>
+      </Accordion.Panel>
+      <Accordion.Panel>
+        <Accordion.Title>What are the differences between Flowbite and Tailwind UI?</Accordion.Title>
+        <Accordion.Content>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+            The main difference is that the core components from Flowbite are open source under the MIT license, whereas
+            Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone
+            components, whereas Tailwind UI offers sections of pages.
+          </p>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+            However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no
+            technical reason stopping you from using the best of two worlds.
+          </p>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+          <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
+            <li>
+              <a href="https://flowbite.com/pro/" className="text-cyan-600 hover:underline dark:text-cyan-500">
+                Flowbite Pro
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://tailwindui.com/"
+                rel="nofollow"
+                className="text-cyan-600 hover:underline dark:text-cyan-500"
+              >
+                Tailwind UI
+              </a>
+            </li>
+          </ul>
+        </Accordion.Content>
+      </Accordion.Panel>
+    </Accordion>
+    </div>
   )
 }
 
